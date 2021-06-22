328e15c145752f38e30ab0ddd125650509572a72
matheuseugeniomoreira1 <matheuseugeniomoreira1@gmail.com.com>
2021-06-22 19:06:17 -0300

    first commit

---
+++ pratica 4/MaxMin.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxMin
+{
+    class MaxMin
+    {
+
+        // Operação relevante: TESTES (if)
+        // T(n) = 2n - 2
+        public void maxMin1(int[] vet)
+        {
+            int ma, me;
+            int i, cont = 0;
+            ma = me = vet[0];
+            for (i = 1; i < vet.Length; i++)
+            {
+                cont++;
+                if (vet[i] < me)
+                    me = vet[i];
+                cont++;
+                if (vet[i] > ma)
+                    ma = vet[i];
+            }
+            Console.Write(cont);
+        }
+
+        // Melhor caso: T(n) = n - 1
+        // Pior caso: T(n) = 2n - 2
+        // Caso médio: T(n) = 3n/2 - 3/2
+        public void maxMin2(int[] vet)
+        {
+            int ma, me, cont = 0;
+            int i;
+            ma = me = vet[0];
+            for (i = 1; i < vet.Length; i++)
+            {
+                cont++;
+                if (vet[i] < me)
+                    me = vet[i];
+
+                else
+                {
+                    cont++;
+                    if (vet[i] > ma)
+                        ma = vet[i];
+                }
+            }
+            Console.Write(cont);
+        }
+
+        // T(n) = 3n/2 - 2
+        public void maxMin3(int[] vet)
+        {
+            int ma, me;
+            int i, cont = 0;
+
+            cont++;
+            if (vet[0] < vet[1])
+            {
+                me = vet[0];
+                ma = vet[1];
+            }
+            else
+            {
+                me = vet[1];
+                ma = vet[0];
+            }
+            for (i = 2; i < vet.Length; i += 2)
+            {
+                cont++;
+                if (vet[i] < vet[i + 1])
+                {
+                    cont++;
+                    if (vet[i] < me)
+                        me = vet[i];
+
+                    cont++;
+                    if (vet[i + 1] > ma)
+                        ma = vet[i + 1];
+                }
+                else
+                {
+                    cont++;
+                    if (vet[i + 1] < me)
+                        me = vet[i + 1];
+
+                    cont++;
+                    if (vet[i] > ma)
+                        ma = vet[i];
+                }
+            }
+            Console.Write(cont);
+        }
+    }
+}
---
+++ pratica 4/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxMin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            MaxMin m = new MaxMin();
+
+            int[] cre = new int[1000];
+            int[] dec = new int[1000];
+            int[] ale = new int[1000];
+
+            for (int i = 0; i < 1000; i++)
+            {
+                cre[i] = i + 1;
+                dec[i] = 1000 - i;
+                ale[i] = r.Next(1, 1000);
+            }
+
+            Console.WriteLine("Maxmin1, crescente: ");
+            m.maxMin1(cre);
+            Console.WriteLine("\nMaxmin1, decrescente: ");
+            m.maxMin1(dec);
+            Console.WriteLine("\nMaxmin1, aleatorio: ");
+            m.maxMin1(ale);
+
+            Console.WriteLine("\nMaxmin2, crescente: ");
+            m.maxMin2(cre);
+            Console.WriteLine("\nMaxmin2, decrescente: ");
+            m.maxMin2(dec);
+            Console.WriteLine("\nMaxmin2, aleatorio: ");
+            m.maxMin2(ale);
+
+            Console.WriteLine("\nMaxmin3, crescente: ");
+            m.maxMin3(cre);
+            Console.WriteLine("\nMaxmin3, decrescente: ");
+            m.maxMin3(dec);
+            Console.WriteLine("\nMaxmin3, aleatorio: ");
+            m.maxMin3(ale);
+            //Console.WriteLine("Maxmin2, crescente: " + m.maxMin2(cre) + ";" + " Decrescente: " + m.maxMin2(dec) + ";" + " Aleatorio: " + m.maxMin2(ale));
+            //Console.WriteLine("Maxmin3, crescente: " + m.maxMin3(cre) + ";" + " Decrescente: " + m.maxMin3(dec) + ";" + " Aleatorio: " + m.maxMin3(ale));
+
+            Console.ReadKey();
+        }
+    }
+}
